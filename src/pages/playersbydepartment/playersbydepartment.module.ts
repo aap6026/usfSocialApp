5beb366d85d8fadf32aeaa88f64e8669b7a1c899
Ille Sutt <ille.sutt@usfoods.com>
2021-07-22 20:52:30 -0500

    Added Shop pages

---
+++ src/pages/playersbydepartment/playersbydepartment.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PlayersbydepartmentPage } from './playersbydepartment';
+
+@NgModule({
+  declarations: [
+    PlayersbydepartmentPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PlayersbydepartmentPage),
+  ],
+})
+export class PlayersbydepartmentPageModule {}
